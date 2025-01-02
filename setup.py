b9a04b26b635e0e17a43e5badd2d8343f0c34bb5
kmoin1309 <moinoddinkazi1234@gmail.com>
2025-01-02 20:39:12 +0530

    feat : Initialization

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Quazi Md Moinuddin',
+    author_email= 'moinoddinkazi13@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
